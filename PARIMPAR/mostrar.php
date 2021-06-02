1a79a64853f48c7dc5d21b54c2cee5f9540d3a2b
StephanySosaGarcia <karensosagarciastephany@gmail.com>
2021-06-02 17:08:57 -0500

    Trabajo grupal

---
+++ PARIMPAR/mostrar.php
@@ -0,0 +1,19 @@
+<?php
+
+
+if(isset($_POST['number'])){
+
+    $numero = intval($_POST['number']);
+
+    if ($numero%2==0){
+        $value = 1;
+    } else {
+        $value = 0;
+    }
+} 
+
+echo $number."value";
+
+header("Location: ./index.php?confirm=".$value)
+
+?>
